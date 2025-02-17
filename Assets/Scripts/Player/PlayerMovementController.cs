1672fd0688e97bac48e5f10ae2d149ee715e4dee
Samuel Lomas <samrlomas@outlook.com>
2025-02-17 16:35:53 +0000

    player movement, texture setup

--- Assets/Scripts/Player/PlayerMovementController.cs
+++ Assets/Scripts/Player/PlayerMovementController.cs
@@ -2,22 +2,44 @@ using UnityEngine;
 
 public class PlayerMovementController : MonoBehaviour
 {
-    public float moveSpeed = 5f;
-    private Rigidbody rigidBodyComponent; 
+    public float moveSpeed = 10f;
+    public float jumpForce = 5f;
+    public float sprintMultiplier = 2f;
+    
+    private Rigidbody rb; 
 
     //Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        rigidBodyComponent = GetComponent<Rigidbody>();
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        float inputAzi = Input.GetAxis("Horizontal");
-        float inputAlt = Input.GetAxis("Vertical");
+        handleJump();
+        handleAxisMovement();
+    }
 
-        Vector3 movementVector = new Vector3(inputAzi, 0f, inputAlt) * moveSpeed;
-        rigidBodyComponent.MovePosition(rigidBodyComponent.position + movementVector * Time.deltaTime);
+    void handleAxisMovement()
+    {
+        Vector2 axis = new Vector2(Input.GetAxis("Vertical"), Input.GetAxis("Horizontal")) 
+            * moveSpeed // Apply Default move speed
+            * (Input.GetKey(KeyCode.LeftShift) ? sprintMultiplier : 1f); // Multiply by sprint multiplier if shift down
+
+        // Determine forward vector from camera's right vector, avoids using rb forward vector which lacks camera sync
+        Vector3 forward = new Vector3(-Camera.main.transform.right.z, 0f, Camera.main.transform.right.x);
+
+        // Combine velocity of all values
+        Vector3 nextVelocity = (forward * axis.x + Camera.main.transform.right * axis.y + Vector3.up * rb.linearVelocity.y);
+        rb.linearVelocity = nextVelocity;
+    }
+
+    void handleJump()
+    {
+        if (Input.GetKey(KeyCode.Space) && Physics.Raycast(rb.transform.position, Vector3.down, 1 + 0.001f))
+        {
+            rb.linearVelocity = new Vector3(rb.linearVelocity.x, jumpForce, rb.linearVelocity.z);
+        }
     }
 }
--- Assets/Scripts/Player/PlayerRotationController.cs
+++ Assets/Scripts/Player/PlayerRotationController.cs
@@ -2,15 +2,27 @@ using UnityEngine;
 
 public class PlayerRotationController : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private float yaw = 0f;
+    private float pitch = 0f;
+
+    public float sensitivity = 7.5f;
+
     void Start()
     {
-        
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // Pitch must be inverted, then clamp it
+        pitch -= Input.GetAxisRaw("Mouse Y") * sensitivity;
+        pitch = Mathf.Clamp(pitch, -90f, 90f);
+
+        yaw += Input.GetAxisRaw("Mouse X") * sensitivity;
+
+        // Set pitch for camera, yaw for object
+        Camera.main.transform.localRotation = Quaternion.Euler(pitch, 0f, 0f);
+        gameObject.transform.rotation = Quaternion.Euler(0f, yaw, 0f);
     }
 }
