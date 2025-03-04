8cfbbac3938dabb53d22f13605b07391f3988644
Samuel Lomas <samrlomas@outlook.com>
2025-03-04 15:57:33 +0000

    Working on collisions

---
+++ Assets/Scripts/Items/PhysicsItemBehaviour.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class PhysicsItemBehaviour : MonoBehaviour
+{
+
+    private Rigidbody2D rigidBodyComponent;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        rigidBodyComponent = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    // True Advertisement
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        this.rigidBodyComponent.linearVelocityY = (-this.rigidBodyComponent.linearVelocityY) * 0.25f;
+    }
+}
